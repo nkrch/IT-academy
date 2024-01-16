bcd2fe8c3d68b29326aaf97a62dedb1eba3b2b5f
nkrch <111977977+nkrch@users.noreply.github.com>
2024-01-16 17:12:13 +0300

    Update treesum.js
    newVersion(reworked)

--- n05/treesum.js
+++ n05/treesum.js
@@ -1,26 +1,24 @@
-var arr = [5, 7, [4, [2], 8, [1, 3], 2],
-    [9, []], 1, 8,
-];
-var sum = 0;
+var arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
 
 function toCount(arra) {
-    for (let i = 0; i < arra.length; i++) {
-        if (typeof (arra[i]) === 'object') {
-            //object
-            toCount(arra[i]);
-        } else {
-            //number
-            if (typeof (arra[i]) !== 'number') {
-                console.log('NO');
-                sum += parseInt(arra[i]);
-                console.log('15' + arra[i]);
-            } else {
-                sum += arra[i];
-            }
-
-        }
-
+  let sum = 0;
+  for (let i = 0; i < arra.length; i++) {
+    if (typeof arra[i] === "object") {
+      //object
+      var newsum = toCount(arra[i]);
+      sum += newsum;
+    } else {
+      //number
+      if (typeof arra[i] !== "number") {
+        console.log("NO");
+        //sum += parseInt(arra[i]);
+        console.log("15" + arra[i]);
+      } else {
+        sum += arra[i];
+      }
     }
+  }
+  return sum;
 }
-toCount(arr);
-console.log(sum);
+
+console.log(toCount(arr));
