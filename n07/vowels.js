f8a61a67e12277427ac28b5513d36fc2e6fc0a9f
nkrch <111977977+nkrch@users.noreply.github.com>
2024-01-16 19:53:39 +0300

    Update vowels.js
    update

--- n07/vowels.js
+++ n07/vowels.js
@@ -1,11 +1,11 @@
 var str = prompt("Введите строку на русском или английском");
 const vowels = "aieouyAIEOUYауоиэыяюеёАУОИЭЫЯЁЕ";
-var vowl = function () {
+var vowl = function (strk, vowel) {
   let vowl = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) vowl++;
+  for (let i = 0; i < strk.length; i++) {
+    if (vowel.includes(strk[i])) vowl++;
   }
   return vowl;
 };
 
-console.log(vowl());
+console.log(vowl(str, vowels));
