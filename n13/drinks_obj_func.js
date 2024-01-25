6f2b75abb18271eb329623a005eef81607c166c1
nkrch <111977977+nkrch@users.noreply.github.com>
2024-01-25 04:31:57 +0300

    Update drinks_obj_func.js

--- n13/drinks_obj_func.js
+++ n13/drinks_obj_func.js
@@ -1,39 +1,35 @@
-allDrinks = {};
-class ObjStorageClass {
-  constructor(name1, alcohol, ingridients) {
-    this.name1 = name1;
-    this.alcohol = alcohol;
-    this.ingridients = ingridients;
-    /*allDrinks[allDrinks.length] = {
-      name1: name1,
-      alcohol: alcohol,
-      ingridients: ingridients,
-    };*/
-  }
+function ObjStorageFunc(name11, alcohol1, ingridients1) {
+  const self = this; // спасаем this и упрощаем себе жизнь
+  drinkStorage = {};
+  this.name1 = name11; // публ. свойство - элемент объекта this/self
+  this.alcohol = alcohol1;
+  this.ingridients = ingridients1;
 
-  addValue(key, value) {
-    allDrinks[key] = value;
-  }
+  this.addValue = function (key, value) {
+    drinkStorage[key] = value;
+  };
 
-  getValue(key) {
+  this.getValue = function (key) {
     // публ. метод - элемент объекта this/self
-    return allDrinks[key];
-  }
+    return drinkStorage[key];
+  };
 
-  deleteValue(key) {
-    delete allDrinks[key];
-    for (let i = key; i + 1 < allDrinks.length; i++) {
-      allDrinks[i] = allDrinks[i + 1];
+  this.deleteValue = function (key) {
+    drinkStorage[key].delete;
+    /*for (let i = key; i + 1 < drinkStorage.length; i++) {
+      drinkStorage[i] = drinkStorage[i + 1];
     }
-    allDrinks[key] = allDrinks[key + 1];
-  }
-
-  getKeys() {
-    return Object.keys(allDrinks);
-  }
+    drinkStorage[key] = drinkStorage[key + 1];*/
+    drinkStorage = drinkStorage.filter(function (el) {
+      return el.name1 != "";
+    });
+  };
+  this.getKeys = function () {
+    return Object.keys(drinkStorage);
+  };
 }
 
-let margarita = new ObjStorageClass();
+let margarita = new ObjStorageFunc();
 /*(this.name1 = ""),
   (margarita.alcohol = true),
   (ingridients = )*/
@@ -41,86 +37,73 @@ let margarita = new ObjStorageClass();
 margarita.name1 = "Маргарита";
 margarita.alcohol = true;
 margarita.ingridients = "продукт, продукт... смешать...";
-allDrinks[allDrinks.length] = {
-  name1: margarita.name1,
-  alcohol: margarita.alcohol,
-  ingridients: margarita.ingridients,
-};
 
-function showAllDrinks() {
-  const docBlock = document.getElementById("show");
-  var textCon = "";
-  allDrinks.forEach((element) => {
-    if (element.alcohol) {
-      var express = "да";
-    } else {
-      var express = "нет";
-    }
-    textCon += `
-    <h3>${element.name1}</h3>
-    напиток ${element.name1}
-    алкогольный: ${express}
-    рецепт приготовления:
-    ${element.ingridients}`;
-  });
-  docBlock.innerHTML = textCon;
-}
+/*<button onclick="inputFunc()">ввод информации о напитке</button>
+            <button onclick="getInfo()">получение информации о напитке</button>
+            <button onclick="deleteThis()">удаление информации о напитке</button>
+            <button onclick="showdrinkStorage()">перечень всех напитков</button>
+ */
 
-function addInfo() {
-  var name1 = prompt("Введите название напитка");
-  var alcohol = confirm("Градусы присутствуют?");
-  var ingridients = prompt("Введите рецепт");
+function inputFunc() {
+  let name1 = prompt("Введите название напитка");
+  let isAlcohol = confirm("Напиток является алкогольным?");
+  let isAlcoholString;
+  isAlcoholString = isAlcohol ? "да" : "нет";
+  let ingridients = prompt("Введите рецепт");
 
-  allDrinks[allDrinks.length] = {
-    name1: name1,
-    alcohol: alcohol,
+  drinkStorage[name1] = {
+    name: name1,
+    alcohol: isAlcoholString,
     ingridients: ingridients,
   };
-  if (alcohol) {
-    var express = "да";
-  } else {
-    var express = "нет";
-  }
-  alert(`напиток ${name1}
-алкогольный: ${express}
-рецепт приготовления:
-${ingridients}`);
-  showAllDrinks();
+  console.log(drinkStorage);
 }
 
 function getInfo() {
-  var check = false;
-  toFind = prompt("Введите название напитка");
-  for (let i = 0; i < allDrinks.length; i++) {
-    if (allDrinks[i].name1 == toFind) {
-      if (allDrinks[i].alcohol) {
-        var express = "да";
-      } else {
-        var express = "нет";
+  let forSearch = prompt("Введите название напитка");
+  let isFound = false;
+  for (const key in drinkStorage) {
+    if (drinkStorage.hasOwnProperty.call(drinkStorage, key)) {
+      if (key == forSearch) {
+        alert(`Название напитка: ${key}
+Алкогольный ли: ${drinkStorage[key].alcohol}
+Рецепт: ${drinkStorage[key].ingridients}`);
+        isFound = true;
       }
-      check = true;
-      alert(`напиток ${allDrinks[i].name1}
-    алкогольный: ${express}
-    рецепт приготовления:
-    ${allDrinks[i].ingridients}`);
     }
   }
-  if (!check) {
-    alert("Нет такого напитка");
+  if (!isFound) {
+    alert("Напиток не найден");
   }
 }
-
-function deleteInfo() {
-  var check = false;
-  console.log(allDrinks);
-  toFind = prompt("Введите название напитка");
-
-  allDrinks = allDrinks.splice(toFind, 1);
-
-  showAllDrinks();
+function showDrinkStorage() {
+  let demonstration = "";
+  for (const key in drinkStorage) {
+    if (drinkStorage.hasOwnProperty.call(drinkStorage, key)) {
+      demonstration += `<h3>${key}</h3>
+      Название напитка: ${key}<br>
+      Алкогольный ли: ${drinkStorage[key].alcohol} <br>
+      Рецепт: ${drinkStorage[key].ingridients}<br>
+      `;
+    }
+  }
+  document.getElementById("show").innerHTML = demonstration;
 }
 
-/*<button onclick="addInfo()">ввод информации о напитке</button>
-            <button onclick="getInfo()">получение информации о напитке</button>
-            <button onclick="deleteInfo()">удаление информации о напитке</button>
-            <button onclick="getAllInfo()">перечень всех напитков</button> */
+function deleteThis() {
+  forSearch = prompt("Что нужно удалить?");
+  let isFound = false;
+  for (const key in drinkStorage) {
+    if (drinkStorage.hasOwnProperty.call(drinkStorage, key)) {
+      if (key == forSearch) {
+        delete drinkStorage[key];
+        isFound = true;
+      }
+    }
+  }
+  if (!isFound) {
+    alert("Напиток не найден");
+  }
+  console.log(drinkStorage);
+  showDrinkStorage();
+}
